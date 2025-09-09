35cb5f903eeaa9e3ffafa44a99abf10d2a2a9acd
Amarnath  Sooraj <166597458+AmarnathSooraj@users.noreply.github.com>
2025-09-09 11:05:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
